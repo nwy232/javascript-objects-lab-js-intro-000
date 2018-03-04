dfcb3f841c19dae47825ef4023bdb4ec8de60d64
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-04 23:09:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ var recipes={}
 
 function updateObjectWithKeyAndValue(object,key,value)
 {
+  return Object.assign({},object,{[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value)
+{
   object[key]=value
+  return object
+}
+
+function deleteFromObjectByKey(object,key)
+{
+  var newobject=object
+  delete newobject[key]
+  return newobject
+}
+
+function destructivelyDeleteFromObjectByKey(object,key)
+{
+  delete object[key]
   return object
 }
